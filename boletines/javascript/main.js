2c1e450e5cf3499c483b22120a71d6b0204c537a
whocar3s <2paula.paez1@gmail.com>
2022-07-03 18:18:14 -0500

    Agregamos el proyecto boletines

---
+++ boletines/javascript/main.js
@@ -0,0 +1,30 @@
+//Obtener los elementos de la clase
+
+let links = document.querySelectorAll(".close");
+
+//Recorrerlos
+
+links.forEach(function(link){
+
+    //Agregar un evento click a cada uno de ellos - case  sensitive
+    link.addEventListener("click", function(ev){
+        ev.preventDefault();
+        let content = document.querySelector('.content');
+
+        //Quitarle las clases de animaciones que ya tiene
+        content.classList.remove("animate__fadeInDown");
+        content.classList.remove("animate__animated");
+
+        //Agregar clases para animar su salida fadeOutUp
+        content.classList.add("animate__fadeOutUp");
+        content.classList.add("animate__animated");
+
+        setTimeout(function () {
+            location.href = "/";
+        },600)
+        //setInterval
+        
+        return false;
+
+    });
+});
