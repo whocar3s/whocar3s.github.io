4adba63f94710ec28d7b9c5d5054e5a53d554fe7
Paula <2paula.paez1@gmail.com>
2022-07-03 19:04:25 -0500

    Redireciona a /boletines

--- boletines/javascript/main.js
+++ boletines/javascript/main.js
@@ -20,7 +20,7 @@ links.forEach(function(link){
         content.classList.add("animate__animated");
 
         setTimeout(function () {
-            location.href = "/";
+            location.href = "/boletines";
         },600)
         //setInterval
         
